e4e745b7f2422d5d2be49e7e921b0b6989873601
Bess <bessfernandez@gmail.com>
2013-05-20 11:20:53 -0400

    mobile tweaks, header animation prototype

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,6 +18,37 @@ require.config({
 require(['app', 'jquery', 'bootstrap', 'underscore'], function (app, $) {
     'use strict';
 
+    $(".scroll").click(function(event){
+         event.preventDefault();
+
+         var dest = 0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest = $(document).height()-$(window).height();
+         } else {
+              dest = $(this.hash).offset().top;
+         }
+
+         //go to destination
+         $('html,body').animate({scrollTop:dest + -50}, 500,'swing');
+     });
+
+   $('header h2, header p, header a').hide();
+   
+
+
+    $('header h2').fadeTo('slow', 1, function() {
+      $("header p").fadeTo('slow', .6);
+      $("header a").fadeTo('slow', .8, function() {
+        $("header p").fadeTo('slow', 1);
+      });
+    });
+
+      
+    
+    
+
+
+    
   
     // use app here
     // console.log(app);
